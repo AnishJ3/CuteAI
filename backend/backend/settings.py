7b70e64ab5ea8821fa8d4255c87d127807d28f6e
AnishJ3 <anish.joshi2021@vitstudent.ac.in>
2024-10-11 17:00:45 +0530

    updated settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,7 +71,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     # "http://localhost:3000",
     "http://127.0.0.1:3000",
-    os.getenv('FRONTEND_URL','http://localhost:3000')
+    os.getenv('FRONTEND_URL','http://localhost:3000'),
+    "https://cuteai-production.up.railway.app"
 ]
 CORS_ALLOW_CREDENTIALS = True  # This line is required to allow credentials
 
